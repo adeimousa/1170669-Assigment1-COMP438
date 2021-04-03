eba7ec6851d64eb16ae0ed1e463de664864e878e
Adei <adeihim99@gmail.com>
2021-04-03 21:25:03 +0300

    Add switch and rating bar

--- app/src/main/java/edu/birzeit/assigment1/MainActivity.java
+++ app/src/main/java/edu/birzeit/assigment1/MainActivity.java
@@ -7,20 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListAdapter;
+import android.widget.RatingBar;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText firstName;
     EditText lastName;
     EditText email;
     EditText phone;
-    EditText dob;
+//    EditText dob;
     EditText password;
     Spinner country;
+    Switch human;
+    RatingBar rating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,21 +49,27 @@ public class MainActivity extends AppCompatActivity {
         lastName = findViewById(R.id.lastName);
         email = findViewById(R.id.email);
         phone = findViewById(R.id.phone);
-        dob = findViewById(R.id.dob);
+//        dob = findViewById(R.id.dob);
         password = findViewById(R.id.password);
+        human = findViewById(R.id.humanView);
+        rating = findViewById(R.id.ratingView);
         Intent intent = new Intent(this, MainActivity2.class);
         String name = firstName.getText().toString() + " " +lastName.getText().toString();
         String email = this.email.getText().toString();
         String phone = this.phone.getText().toString();
-        String dob = this.dob.getText().toString();
+//        String dob = this.dob.getText().toString();
         String password = this.password.getText().toString();
         String country = this.country.getSelectedItem().toString();
+        boolean human = this.human.isChecked();
+        float rating = this.rating.getRating();
         intent.putExtra("name",name );
         intent.putExtra("email",email );
         intent.putExtra("phone",phone );
-        intent.putExtra("dob",dob );
+//        intent.putExtra("dob",dob );
         intent.putExtra("password",password );
         intent.putExtra("country",country );
+        intent.putExtra("human",human );
+        intent.putExtra("rating",rating );
         startActivity(intent);
     }
 }
--- app/src/main/java/edu/birzeit/assigment1/MainActivity2.java
+++ app/src/main/java/edu/birzeit/assigment1/MainActivity2.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
     TextView nameView;
     TextView emailView;
     TextView phoneView;
-    TextView dobView;
+//    TextView dobView;
     TextView countryView;
-    TextView passwordView;
+    TextView humanView;
+    RatingBar ratingView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,20 +25,25 @@ public class MainActivity2 extends AppCompatActivity {
         String name = intent.getStringExtra("name");
         String email = intent.getStringExtra("email");
         String phone = intent.getStringExtra("phone");
-        String dob = intent.getStringExtra("dob");
-        String password = intent.getStringExtra("password");
+//        String dob = intent.getStringExtra("dob");
+//        String password = intent.getStringExtra("password");
         String country = intent.getStringExtra("country");
+        boolean isHuman = intent.getBooleanExtra("human", true);
+        float rating = intent.getFloatExtra("rating", 0);
         nameView = findViewById(R.id.nameView);
         emailView = findViewById(R.id.emailView);
         phoneView = findViewById(R.id.phoneView);
-        dobView = findViewById(R.id.dobView);
-        passwordView = findViewById(R.id.passwordView);
+//        dobView = findViewById(R.id.dobView);
         countryView = findViewById(R.id.countryView);
+        humanView = findViewById(R.id.humanView);
+        ratingView = findViewById(R.id.ratingView);
         nameView.setText(name);
         emailView.setText(email);
         phoneView.setText(phone);
-        dobView.setText(dob);
+//        dobView.setText(dob);
         countryView.setText(country);
-        passwordView.setText(password);
+        humanView.setText("Is Human: " + isHuman );
+        ratingView.setRating(rating);
+        ratingView.setIsIndicator(true);
     }
 }
